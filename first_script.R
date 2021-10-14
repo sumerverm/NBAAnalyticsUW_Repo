7613262b53d2e1bf6e5257b039b2199955581fee
Sumer Verma <Sumer@MacBook-Pro.local>
2021-10-14 13:51:03 -0700

    Second Commit

---
+++ first_script.R
@@ -0,0 +1,27 @@
+#install packages
+install.packages("tidyverse")
+install.packages("nbastatR")
+#The jsonlite package is a JSON parser/generator optimized for the web. 
+#Its main strength is that it implements a bidirectional mapping 
+#between JSON data and the most important R data types.
+install.packages("jsonlite")
+#janitor package is a R package that has simple functions for examining and cleaning dirty data
+install.packages("janitor")
+#Make true type fonts easy to use with pdf
+install.packages("extrafont")
+#repel overlapping text labels
+install.packages("ggrepel")
+#Scale customize appearance of axis and legend labels on graphs
+install.packages("scales")
+#team colors provides color pallate for pro sports teams
+install.packages("teamcolors")
+#zoo  class with methods for totally ordered indexed observations, vectors matrices and factors
+install.packages("zoo")
+#future provide a lightweight and unified Future API 
+#for sequential and parallel processing of R expression via futures.
+install.packages("future")
+#Lubridate makes it easier to do the things 
+#R does with date-times and possible to do the things R does not
+install.packages("lubridate")
+
+
