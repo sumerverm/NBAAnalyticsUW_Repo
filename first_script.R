a0590a446e2d601ead94854dad00eefbe5df2b94
Sumer Verma <Sumer@MacBook-Pro.local>
2021-10-14 15:08:54 -0700

    Third Commit *added R markdown *loaded Packages following tutorial *

--- first_script.R
+++ first_script.R
@@ -1,9 +1,15 @@
 #install packages
 install.packages("tidyverse")
-install.packages("nbastatR")
+
+install.packages("devtools")
+devtools::install_github("abresler/nbastatR")
+
+install.packages("BasketballAnalyzeR")
+
 #The jsonlite package is a JSON parser/generator optimized for the web. 
 #Its main strength is that it implements a bidirectional mapping 
 #between JSON data and the most important R data types.
+
 install.packages("jsonlite")
 #janitor package is a R package that has simple functions for examining and cleaning dirty data
 install.packages("janitor")
@@ -40,4 +46,32 @@ library(teamcolors)
 library(zoo)
 library(future)
 library(lubridate)
+
+Sys.setenv("VROOM_CONNECTION_SIZE" = 131072 * 2)
+
+#Get Game ID's and Gamelog Data
+
+# Select seasons from 1949 and after
+selectedSeasons <- c(1996:1998)
+# Get game IDs for Regular Season and Playoffs
+gameIds_Reg <- suppressWarnings(seasons_schedule(seasons = selectedSeasons, season_types = "Regular Season") %>% select(idGame, slugMatchup))
+gameIds_PO <- suppressWarnings(seasons_schedule(seasons = selectedSeasons, season_types = "Playoffs") %>% select(idGame, slugMatchup))
+gameIds_all <- rbind(gameIds_Reg, gameIds_PO)
+# Peek at the game IDs
+head(gameIds_all)
+tail(gameIds_all)
+#####################
+## Retrieve gamelog data for players and teams
+#####################
+# Get player gamelogs
+P_gamelog_reg <- suppressWarnings(game_logs(seasons = selectedSeasons, league = "NBA", result_types = "player", season_types = "Regular Season"))
+P_gamelog_po <- suppressWarnings(game_logs(seasons = selectedSeasons, league = "NBA", result_types = "player", season_types = "Playoffs"))
+P_gamelog_all <- rbind(P_gamelog_reg, P_gamelog_po)
+View(head(P_gamelog_all))
+# Get team gamelogs
+T_gamelog_reg <- suppressWarnings(game_logs(seasons = selectedSeasons, league = "NBA", result_types = "team", season_types = "Regular Season"))
+T_gamelog_po <- suppressWarnings(game_logs(seasons = selectedSeasons, league = "NBA", result_types = "team", season_types = "Playoffs"))
+T_gamelog_all <- rbind(T_gamelog_reg, T_gamelog_po)
+View(head(T_gamelog_all))
+
 
