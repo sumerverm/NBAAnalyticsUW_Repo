4409b439c7cc60414e693d340963d9982db23928
Sumer Verma <Sumer@MacBook-Pro.local>
2021-10-21 12:47:53 -0700

    Michael Jordan Bulls Visualization

--- first_script.R
+++ first_script.R
@@ -52,7 +52,7 @@ Sys.setenv("VROOM_CONNECTION_SIZE" = 131072 * 2)
 #Get Game ID's and Gamelog Data
 
 # Select seasons from 1949 and after
-selectedSeasons <- c(2019:2020)
+selectedSeasons <- c(1996:1998)
 # Get game IDs for Regular Season and Playoffs
 gameIds_Reg <- suppressWarnings(seasons_schedule(seasons = selectedSeasons, season_types = "Regular Season") %>% select(idGame, slugMatchup))
 gameIds_PO <- suppressWarnings(seasons_schedule(seasons = selectedSeasons, season_types = "Playoffs") %>% select(idGame, slugMatchup))
@@ -74,4 +74,56 @@ T_gamelog_po <- suppressWarnings(game_logs(seasons = selectedSeasons, league = "
 T_gamelog_all <- rbind(T_gamelog_reg, T_gamelog_po)
 View(head(T_gamelog_all))
 
+#####################
+## Use Regular Season data
+#####################
+# Create Tbox (Team boxscore) for each Regular Season
+Tbox <- T_gamelog_reg %>%
+  group_by("Season"=yearSeason, "Team"=slugTeam) %>%
+  dplyr::summarise(GP=n(), MIN=sum(round(minutesTeam/5)),
+                   PTS=sum(ptsTeam),
+                   W=sum(outcomeGame=="W"), L=sum(outcomeGame=="L"),
+                   P2M=sum(fg2mTeam), P2A=sum(fg2aTeam), P2p=P2M/P2A,
+                   P3M=sum(fg3mTeam), P3A=sum(fg3aTeam), P3p=P3M/P3A,
+                   FTM=sum(ftmTeam), FTA=sum(ftaTeam), FTp=FTM/FTA,
+                   OREB=sum(orebTeam), DREB=sum(drebTeam), AST=sum(astTeam),
+                   TOV=sum(tovTeam), STL=sum(stlTeam), BLK=sum(blkTeam),
+                   PF=sum(pfTeam), PM=sum(plusminusTeam)) %>%
+  as.data.frame()
+# Create Obox (Opponent Team boxscore) for each Regular Season
+Obox <- T_gamelog_reg %>%
+  group_by("Season"=yearSeason, "Team"=slugOpponent) %>%
+  dplyr::summarise(GP=n(), MIN=sum(round(minutesTeam/5)),
+                   PTS=sum(ptsTeam),
+                   W=sum(outcomeGame=="L"), L=sum(outcomeGame=="W"),
+                   P2M=sum(fg2mTeam), P2A=sum(fg2aTeam), P2p=P2M/P2A,
+                   P3M=sum(fg3mTeam), P3A=sum(fg3aTeam), P3p=P3M/P3A,
+                   FTM=sum(ftmTeam), FTA=sum(ftaTeam), FTp=FTM/FTA,
+                   OREB=sum(orebTeam), DREB=sum(drebTeam), AST=sum(astTeam),
+                   TOV=sum(tovTeam), STL=sum(stlTeam), BLK=sum(blkTeam),
+                   PF=sum(pfTeam), PM=sum(plusminusTeam)) %>%
+  as.data.frame()
+# Create Pbox (Player boxscore) for each Regular Season
+Pbox <- P_gamelog_reg %>%
+  group_by("Season"=yearSeason, "Team"=slugTeam, "Player"=namePlayer) %>%
+  dplyr::summarise(GP=n(), MIN=sum(minutes), PTS=sum(pts),
+                   P2M=sum(fg2m), P2A=sum(fg2a), P2p=100*P2M/P2A,
+                   P3M=sum(fg3m), P3A=sum(fg3a), P3p=100*P3M/P3A,
+                   FTM=sum(ftm), FTA=sum(fta), FTp=100*FTM/FTA,
+                   OREB=sum(oreb), DREB=sum(dreb), AST=sum(ast),
+                   TOV=sum(tov), STL=sum(stl), BLK=sum(blk),
+                   PF=sum(pf)) %>%
+  as.data.frame()
+View(Pbox[Pbox$Player=="Michael Jordan",])
 
+# Bar plots
+#####################
+
+teamSelected <- "CHI"
+Pbox.sel <- subset(Pbox, Team==teamSelected &
+                     MIN>=1000)
+seasonSelected <- 1998
+barline(data=Pbox.sel[Pbox.sel$Season==seasonSelected,], id="Player",
+        bars=c("P2M","P3M","FTM"), line="PTS",
+        order.by="PTS", labels.bars=c("2PM","3PM","FTM"),
+        title=teamSelected)
